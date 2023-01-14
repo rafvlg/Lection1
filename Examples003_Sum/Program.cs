7cee4d9501bf5e788d7a77c64ffda4547b24efac
rafvlg <121390991+rafvlg@users.noreply.github.com>
2023-01-14 16:13:36 +0300

    Random

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,4 +1,11 @@
-﻿int numberA = 150;
-int numberB = 150;
+﻿int numberA = new Random().Next(1,10); // Случайное число от 1.....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
+
+// int numberA = 33;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
